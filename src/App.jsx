2f1dd6e8da24039d6780459e5efda6a0db1c0e6e
unknown <kellskelechi@gmail.com>
2023-06-28 14:54:18 +0100

    folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 
 import React from 'react'
+import Navbar from "./sections/navbar/Navbar"
+import Header from "./sections/header/Header"
+import About from "./sections/about/About"
+import Services from "./sections/services/Services"
+import Contact from "./sections/Contact/Contact"
+import Footer from "./sections/footer/Footer"
+import FloatingNav from "./sections/floating-nav/FloatingNav"
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Services/>
+      <Contact/>
+      <Footer/>
+      <FloatingNav/>
+    </main>
   )
 }
 
---
+++ src/sections/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./header.css"
+const Header = () => {
+  return (
+    <header id='header'>Header</header>
+  )
+}
+
+export default Header
