7b1aa8faced2a6f22dd7ba70cce51148849b4726
unknown <kellskelechi@gmail.com>
2023-07-03 12:57:30 +0100

    header

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import "./header.css"
-import headerImg from "../../assets/desktop.png"
+import data from "./data"
+import headerImg from "../../assets/header.png"
 const Header = () => {
   return (
     <header id='header'>
       <div className="container header-container">
         <div className="header-section">
-          <img src={headerImg} alt="" />
+          <img src={headerImg} alt="header-picture" />
+        </div>
+        <h3>FAVOUR KELECHI</h3>
+
+        <p> Creating web interfaces that adapt and function seamlessly across multiple devices and screen sizes, ensuring an optimal user experience.</p>
+        <div className="header-cta">
+          <a href="#contact">Let's talk</a>
+          <a href="#portfolio">My work</a>
         </div>
 
+        <div className="header-socials">
+
+        </div>
       </div>
-      </header>
+
+    </header>
   )
 }
 
---
+++ src/sections/header/data.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {BsTwitter,BsLinkedin, BsGithub} from "react-icons/bs";
+
+
+const data =[
+    
+    {id: 1, link:"https://twitter.com/KelechiKells", icon: "<BsTwitter />"},
+    {id: 2, link: "https://www.linkedin.com/in/kelechifavour01/", icon: "<BsLinkedin/>"},
+    {id: 3, link:"https://github.com/Kelly-kells/", icon: "<BsGithub />"},
+]
+export default data;
