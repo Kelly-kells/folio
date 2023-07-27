9f4aad8219b431ffd9a505cd7ce28b3b5aed1334
unknown <kellskelechi@gmail.com>
2023-07-28 00:19:54 +0100

    header-section finished

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -10,27 +10,20 @@ const Header = () => {
           <div className='header-img'>
             <img src={headerImg} alt="header-picture" />
           </div>
-
         </div>
         <div className='header-contents'>
           <h3>FAVOUR KELECHI</h3>
-
-          <p> An experienced frontend developr that is Committed to creating an accessible web, ensuring all developed features comply with industry standards and web accessibility guidelines.Creating web interfaces that adapt and function seamlessly across multiple devices and screen sizes, ensuring an optimal user experience.</p>
+          <p> An experienced frontend developer that is Committed to creating an accessible web, ensuring all developed features comply with industry standards and web accessibility guidelines.Creating web interfaces that adapt and function seamlessly across multiple devices and screen sizes, ensuring an optimal user experience.</p>
           <div className="header-cta">
             <a href="#contact" className='btn primary'>Let's talk</a>
             <a href="#portfolio" className='btn white'>My work</a>
           </div>
-
-
           <div className="header-socials">
             {
               data.map(item => <a key={item.id} href={item.link} target='_blank' rel='noopener noreferer'>{item.icon}</a>)
             }
-
           </div>
         </div>
-
-
       </div>
 
     </header>
