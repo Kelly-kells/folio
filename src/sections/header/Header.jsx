f80cc46ecb31f0720675a42e22f560dd12e670c5
unknown <kellskelechi@gmail.com>
2023-07-27 20:34:55 +0100

    cta

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -7,15 +7,18 @@ const Header = () => {
     <header id='header'>
       <div className="container header-container">
         <div className="header-profile">
-          <img src={headerImg} alt="header-picture" />
+          <div className='header-img'>
+            <img src={headerImg} alt="header-picture" />
+          </div>
+
         </div>
         <div className='header-contents'>
           <h3>FAVOUR KELECHI</h3>
 
           <p> An experienced frontend developr that is Committed to creating an accessible web, ensuring all developed features comply with industry standards and web accessibility guidelines.Creating web interfaces that adapt and function seamlessly across multiple devices and screen sizes, ensuring an optimal user experience.</p>
           <div className="header-cta">
-            <a href="#contact">Let's talk</a>
-            <a href="#portfolio">My work</a>
+            <a href="#contact" className='btn primary'>Let's talk</a>
+            <a href="#portfolio" className='btn white'>My work</a>
           </div>
 
 
