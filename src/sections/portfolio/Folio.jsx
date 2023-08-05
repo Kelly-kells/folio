fefb4580248b659d6470627fa9ffee397c7fb457
unknown <kellskelechi@gmail.com>
2023-08-05 16:59:13 +0100

    folio-images

--- src/sections/portfolio/Folio.jsx
+++ src/sections/portfolio/Folio.jsx
@@ -3,6 +3,7 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+import fit from "../../assets/fitApp.png"
 
 // Import Swiper styles
 import 'swiper/css';
@@ -10,36 +11,39 @@ import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 
 import './portfolio.css';
-
-// import required modules
 import { FreeMode, Pagination } from 'swiper/modules';
-
-const Folio = () =>{
+const Folio = () => {
   return (
     <section id='portfolio'>
+      <h2> My SKills</h2>
+      <p>I possess the following skills:</p>
       <div className="container folio-container">
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="folio-contents">
+              <div className="folio-image">
+                <img src={fit} alt="" />
+              </div>
+              <div className="folio-cta">
+                <a className="btn white" href="https://github.com/Kelly-kells/fit-fam" target='_blank'>Github</a>
+                <a className='btn primary' href="https://marchfitness.netlify.app" target='_blank'>Link </a>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+        </Swiper>
       </div>
-      
     </section>
   );
 }
