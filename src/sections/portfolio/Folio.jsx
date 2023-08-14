871191b21225975be8bfd02aacad8cf58b66c10c
unknown <kellskelechi@gmail.com>
2023-08-14 14:01:23 +0100

    updated

--- src/sections/portfolio/Folio.jsx
+++ src/sections/portfolio/Folio.jsx
@@ -1,107 +1,76 @@
 
-
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import pot from "../../assets/africanpot.jpeg"
+import film from "../../assets/flimore.jpeg"
 import fit from "../../assets/fit.png"
 import ai from "../../assets/projectai.jpeg"
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
-
 import './portfolio.css';
-import { FreeMode, Pagination } from 'swiper/modules';
+
 const Folio = () => {
   return (
     <section id='portfolio'>
       <h2> My Work</h2>
       <p> Projects I've worked on</p>
       <div className="container folio-container">
-        <Swiper
-          slidesPerView={3}
-          spaceBetween={15}
-          freeMode={true}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[FreeMode, Pagination]}
-          className="mySwiper"
-        >
-          <SwiperSlide className='swipe-container'>
+          <div className='swiper-folio'>
             <div className="folio-contents">
               <div className="folio-image">
-                <img src={fit} alt="" />
+                <img src={fit} alt="" /> 
+                <article>
+
+                </article>
+              </div>
+              <div className="folio-cta">
+                  <a className=" btn primary" href="https://github.com/Kelly-kells/fit-fam" target='_blank'>Github</a>
+                  <a className=' btn white' href="https://marchfitness.netlify.app" target='_blank'>Demo </a>
+                </div>
+            </div>
+          </div>
+
+        <div>
+            <div className="swiper-folio">
+              <div className="folio-contents">
+                <div className="folio-image">
+                  <img src={ai} alt="" />
+
+                  <div className="folio-cta">
+                    <a className=" btn primary" href="https://github.com/Kelly-kells/African-spot" target='_blank'>Github</a>
+                    <a className=' btn white' href="https://marchfitness.netlify.app" target='_blank'>Demo </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          
+          <div>
+            <div className="swiper-folio">
+              <div className="folio-contents">
+                <div className="folio-image">
+                  <img src={film} alt="" />
+                </div>
                 <div className="folio-cta">
-                  <a className=" white button" href="https://github.com/Kelly-kells/fit-fam" target='_blank'>Github</a>
-                  <a className=' button' href="https://marchfitness.netlify.app" target='_blank'>Link </a>
-                </div>
+                    <a className=" btn primary" href="https://github.com/Kelly-kells/African-spot" target='_blank'>Github</a>
+                    <a className=' btn white' href="https://marchfitness.netlify.app" target='_blank'>Demo </a>
+                  </div>
               </div>
             </div>
-          </SwiperSlide>
 
-        <SwiperSlide>
+          </div>
+          <div>
             <div className="swiper-folio">
               <div className="folio-contents">
                 <div className="folio-image">
-                  <img src={ai} alt="" />
+                  <img src={pot} alt="" />
 
                   <div className="folio-cta">
-                    <a className=" button" href="https://github.com/Kelly-kells/African-spot" target='_blank'>Github</a>
-                    <a className=' button' href="https://marchfitness.netlify.app" target='_blank'>Link </a>
+                    <a className=" btn primary" href="https://github.com/Kelly-kells/African-spot" target='_blank'>Github</a>
+                    <a className=' btn white' href="https://marchfitness.netlify.app" target='_blank'>Demo </a>
                   </div>
                 </div>
               </div>
             </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className="swiper-folio">
-              <div className="folio-contents">
-                <div className="folio-image">
-                  <img src={ai} alt="" />
-
-                  <div className="folio-cta">
-                    <a className=" button" href="https://github.com/Kelly-kells/African-spot" target='_blank'>Github</a>
-                    <a className=' button' href="https://marchfitness.netlify.app" target='_blank'>Link </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="swiper-folio">
-              <div className="folio-contents">
-                <div className="folio-image">
-                  <img src={ai} alt="" />
-
-                  <div className="folio-cta">
-                    <a className=" button" href="https://github.com/Kelly-kells/African-spot" target='_blank'>Github</a>
-                    <a className=' button' href="https://marchfitness.netlify.app" target='_blank'>Link </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="swiper-folio">
-              <div className="folio-contents">
-                <div className="folio-image">
-                  <img src={ai} alt="" />
-
-                  <div className="folio-cta">
-                    <a className=" button" href="https://github.com/Kelly-kells/African-spot" target='_blank'>Github</a>
-                    <a className=' button' href="https://marchfitness.netlify.app" target='_blank'>Link </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-
-        </Swiper>
+          </div>
       </div>
     </section>
   );
