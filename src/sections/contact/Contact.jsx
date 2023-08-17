3df92fa80e651a629b28ed81442710b034e8730d
unknown <kellskelechi@gmail.com>
2023-08-17 22:48:27 +0100

    contact-option

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import {GrMailOption}from "react-icons/gr"
+import { GrMailOption } from "react-icons/gr"
+import { BsTwitter } from "react-icons/bs"
+import { } from "react-icons/ai"
+
 
 import "./contact.css"
 
@@ -11,30 +14,30 @@ const Contact = () => {
       <div className="container contact-container">
         <div className="contact-options">
           <article className="contact-option">
-            <GrMailOption/>
+            <GrMailOption />
             <h4>Email</h4>
             <h5>kellskelechi@gmail.com</h5>
-             <a href="mailto:Kellskelechi@gmail.com">Send a message</a>
+            <a href="mailto:Kellskelechi@gmail.com">Send a message</a>
           </article>
 
           <article className="contact-option">
-            <GrMailOption/>
-            <h4>Email</h4>
-            <h5>kellskelechi@gmail.com</h5>
-             <a href="mailto:Kellskelechi@gmail.com">Send a message</a>
+            <AiFillLinkedin />
+            <h4>LinkedIn</h4>
+            <h5>Kelechi FAvour</h5>
+            <a href="https://www.linkedin.com/in/kelechifavour01/">Send a message</a>
           </article>
 
           <article className="contact-option">
-            <GrMailOption/>
-            <h4>Email</h4>
-            <h5>kellskelechi@gmail.com</h5>
-             <a href="mailto:Kellskelechi@gmail.com">Send a message</a>
+            <BsTwitter />
+            <h4>Twitter</h4>
+            <h5>Dev Kelly</h5>
+            <a href="https://twitter.com/KelechiKells">Send a message</a>
           </article>
         </div>
         <form action=""></form>
       </div>
-      
-      </section>
+
+    </section>
   )
 }
 
