6a9adff2ac6e7ee83384dc17786f85b626386846
unknown <kellskelechi@gmail.com>
2023-08-18 13:08:52 +0100

    styling

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GrMailOption } from "react-icons/gr"
+import { HiMail } from "react-icons/hi"
 import { BsTwitter } from "react-icons/bs"
 import {AiFillLinkedin } from "react-icons/ai"
 
@@ -14,21 +14,21 @@ const Contact = () => {
       <div className="container contact-container">
         <div className="contact-options">
           <article className="contact-option">
-            <GrMailOption />
+            <HiMail className='option-icon'/>
             <h4>Email</h4>
             <h5>kellskelechi@gmail.com</h5>
             <a href="mailto:Kellskelechi@gmail.com" target='_blank'> Send a message</a>
           </article>
 
           <article className="contact-option">
-            <AiFillLinkedin />
+            <AiFillLinkedin className='option-icon' />
             <h4>LinkedIn</h4>
             <h5>Kelechi FAvour</h5>
             <a href="https://www.linkedin.com/in/kelechifavour01/" target='_blank'>Send a message</a>
           </article>
 
           <article className="contact-option">
-            <BsTwitter />
+            <BsTwitter className='option-icon'/>
             <h4>Twitter</h4>
             <h5>Dev Kelly</h5>
             <a href="https://twitter.com/KelechiKells" target='_blank'>Send a message</a>
