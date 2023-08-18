3f8aa17e1393f188972b8bb92eeba43889f30974
unknown <kellskelechi@gmail.com>
2023-08-18 15:49:20 +0100

    section-finished

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,7 +1,7 @@
-import React, {useRef} from 'react'
+import React, { useRef } from 'react'
 import { HiMail } from "react-icons/hi"
 import { BsTwitter } from "react-icons/bs"
-import {AiFillLinkedin } from "react-icons/ai"
+import { AiFillLinkedin } from "react-icons/ai"
 
 import emailjs from '@emailjs/browser';
 
@@ -15,12 +15,13 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('service_egyifda', 'template_pvgemhr', form.current, 'y5ypNlRhu016d7tFI')
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
+    e.target.reset()
   };
   return (
     <section id='contact'>
@@ -29,7 +30,7 @@ const Contact = () => {
       <div className="container contact-container">
         <div className="contact-options">
           <article className="contact-option">
-            <HiMail className='option-icon'/>
+            <HiMail className='option-icon' />
             <h4>Email</h4>
             <h5>kellskelechi@gmail.com</h5>
             <a href="mailto:Kellskelechi@gmail.com" target='_blank'> Send a message</a>
@@ -43,7 +44,7 @@ const Contact = () => {
           </article>
 
           <article className="contact-option">
-            <BsTwitter className='option-icon'/>
+            <BsTwitter className='option-icon' />
             <h4>Twitter</h4>
             <h5>Dev Kelly</h5>
             <a href="https://twitter.com/KelechiKells" target='_blank'>Send a message</a>
@@ -52,8 +53,8 @@ const Contact = () => {
         <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='Name' placeholder='Your Full Name' required />
           <input type="text" name='email' placeholder='Your Email' required />
-          <textarea name="message"  rows="8" placeholder='Your Message' required></textarea>
-          <button type='submit'value="send" className='btn primary'>Send message</button>
+          <textarea name="message" rows="8" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn primary'>Send message</button>
         </form>
       </div>
 
